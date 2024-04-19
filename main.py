7345fe4b27fa733d077f6480a7bb3c3623b77052
V <119004237+V2Valerie@users.noreply.github.com>
2024-04-19 22:42:07 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import os
 from PIL import Image
 
-im = "yourPath"
+def convert_to_png(directory_path):
+    for r, _, files in os.walk(directory_path):
+        i = 0
+        for file in files:
+            filepath = os.path.join(r, file)
+            try:
+                img = Image.open(filepath)
+                image_resized = img.resize((512, 512))
+                image_resized.save(f'{os.path.splitext(filepath)[0]}.png')
+                i += 1
+                print(f"Converted {os.path.basename(filepath)} to PNG")
+            except Exception as e:
+                print(f"Error converting {os.path.basename(filepath)}: {e}")
 
-for r, _, files in os.walk(im):
-    i = 0
-    for file in files:
-        filepath = os.path.join(r, file)
-        try:
-            img = Image.open(filepath)
-            image_resized = img.resize((512, 512))
-            image_resized.save(f'test_{i}.png')
-            i = i + 1
-            print(i)
-        except Exception as e:
-            print(f"Error opening {filepath}: {e}")
+# Example usage:
+directory_path = "your_directory_path_here"
+convert_to_png(directory_path)
